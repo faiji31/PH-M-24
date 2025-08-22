9231980e22a5996ff885f34871741f0053fe775b
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-22 11:10:47 +0600

    16th commit

---
+++ faiji.js
@@ -0,0 +1,10 @@
+const newChild = document.createElement('li');
+newChild.innerText = "CR9";
+const playerList = document.getElementById("Player_list");
+
+
+
+
+
+
+playerList.appendChild(newChild);
