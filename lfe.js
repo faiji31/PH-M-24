182d9fb21dc7d383c5b25afe1df3e86518b0caf2
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-17 23:55:14 +0600

    4th ommit

--- lfe.js
+++ lfe.js
@@ -1,10 +1,14 @@
 const licollection = document.getElementsByTagName('li')
 console.log(licollection)
 for (const li of licollection){
-    console.log(li.innerText);
+    // console.log(li.innerText);
 }
 
 const allheadings = document.getElementsByTagName('h1');
 for (const head of allheadings){
-    console.log(head.innerText)
+    // console.log(head.innerText)
 }
+
+const foodtitle = document.getElementById('food-name')
+console.log(foodtitle.innerText)
+foodtitle.innerText = "Food i loved most"
